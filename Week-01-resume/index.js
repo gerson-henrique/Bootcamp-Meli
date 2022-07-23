e17d640dff2d5f0f96f818bad342b3c11e79a90b
gerson-henrique <gerson.almeida@mercadolivre.com>
2022-07-23 13:39:33 -0300

    feat : stub alternative in index line 4

---
+++ Week-01-resume/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+let api = require("./products")
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const { products } = req.body;
+  products.forEach((product) => {
+    api.push(product);
+  });
+  res.status(201).send(products);
+});
+
+app.put("/", (req, res) => {
+  const { payload, id } = req.body;
+  api.forEach((product, index) => {
+    if (product.id === id) {
+      api[index] = payload;
+    }
+  });
+  res.status(201).end();
+});
+
+app.delete("/", (req, res) => {
+  const { id } = req.body;
+  const newApi = api.filter((product) => product.id === id);
+  const api = newApi;
+  res.status(201).end();
+});
+
+app.get("/", (req, res) => res.json(api));
+app.listen(port, () => console.log(` listening on port ${port}!`));
